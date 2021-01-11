98401df1903878db49db5370a7c4180ac70b64b1
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2021-01-10 22:14:13 -0300

    Header component: add open modal action

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Button } from 'antd';
 
@@ -6,14 +7,21 @@ import { PlusOutlined } from '@ant-design/icons';
 
 import styles from './Header.module.css';
 import logo from '../../assets/logo.png';
+import { openModal } from '../../store/modules/tickets/actions';
 
 // const { Header } = Layout;
 
 export default function HeaderComponent() {
+  const dispatch = useDispatch();
+
+  const handleAddTicket = () => {
+    dispatch(openModal());
+  };
+
   return (
     <header className={styles.root}>
       <img src={logo} alt="Trílogo" />
-      <Button type="primary" icon={<PlusOutlined />} size={10} shape="round">
+      <Button type="primary" icon={<PlusOutlined />} size={10} shape="round" onClick={handleAddTicket}>
         Novo ticket
       </Button>
     </header>
