0f0120812c52a9ca3494d07cdd04de848f4151e7
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2021-01-12 14:26:43 -0300

    add persist redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import Home from './pages/Home';
 import './App.less';
 import './styles/global.css';
 
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Home />
+      <PersistGate loading={null} persistor={persistor}>
+        <Home />
+      </PersistGate>
     </Provider>
   );
 }
