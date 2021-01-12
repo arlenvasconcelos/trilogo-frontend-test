153b7fa8c5c2501de9bdead72b787660f7ffb073
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2021-01-12 13:39:46 -0300

    Ticket component: add remove ticket

--- src/components/Ticket/index.jsx
+++ src/components/Ticket/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
   Badge, Card, Typography, Space, Button, Image,
@@ -7,28 +8,34 @@ import {
 
 import { EllipsisOutlined } from '@ant-design/icons';
 import TicketModal from '../TicketModal';
-// import styles from './Ticket.module.css';
+import styles from './Ticket.module.css';
+
+import { removeTicket } from '../../store/modules/tickets/actions';
 
 const { Text } = Typography;
 
 export default function Ticket({ ticket }) {
   const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
 
   const handleUpdateTicket = () => {
     setShowModal(true);
+  };
+  const handleDeleteTicket = () => {
+    dispatch(removeTicket({ ticketId: ticket.id }));
   };
 
   const menu = (
     <Menu>
       <Menu.Item>
-        <button onClick={handleUpdateTicket} type="button">
+        <button onClick={handleUpdateTicket} type="button" className={styles.dropdpwnButton}>
           Editar
         </button>
       </Menu.Item>
       <Menu.Item>
-        <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        <button onClick={handleDeleteTicket} type="button" className={styles.dropdpwnButton}>
           Excluir
-        </a>
+        </button>
       </Menu.Item>
     </Menu>
   );
@@ -53,7 +60,7 @@ export default function Ticket({ ticket }) {
               color: '#1F1F49',
             }}
           />
-          <Text strong>{ticket.id }</Text>
+          <Text strong>{ticket.code }</Text>
           <Text type="secondary">{ticket.description}</Text>
           <Space size={85} style={{ marginTop: '15px' }}>
             <Text>{ticket.manage}</Text>
@@ -84,5 +91,6 @@ Ticket.propTypes = {
     manage: PropTypes.string,
     image: PropTypes.string,
     status: PropTypes.string,
+    code: PropTypes.number,
   }).isRequired,
 };
